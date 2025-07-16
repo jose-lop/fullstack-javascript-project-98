4eed863ef508c3ecaded2acc0e0036c2791de70c
jose-lop <jose.ant1022@gmail.com>
2025-07-16 09:54:07 -0500

    agregar juego de verificación de paridad (brain-even)

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 [![Actions Status](https://github.com/jose-lop/fullstack-javascript-project-98/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/jose-lop/fullstack-javascript-project-98/actions)
 
 [![Maintainability](https://qlty.sh/gh/jose-lop/projects/fullstack-javascript-project-98/maintainability.svg)](https://qlty.sh/gh/jose-lop/projects/fullstack-javascript-project-98)
+
+## 🎮 Demostración del juego brain-even
+
+https://asciinema.org/a/asQ63XgjvZg7NmxdNeJ0Ssuw1
---
+++ src/games/even.js
@@ -0,0 +1,39 @@
+import readlineSync from 'readline-sync';
+
+const isEven = (num) => num % 2 === 0;
+
+const runEvenGame = () => {
+  console.log('¡Bienvenido a Brain Games!');
+  const name = readlineSync.question('¿Cual es tu nombre? ');
+  console.log(`¡Hola, ${name}!`);
+  console.log('Responde "yes" si el numero es par, de lo contrario responde "no".');
+
+  let correctAnswers = 0;
+
+  while (correctAnswers < 3) {
+    const number = Math.floor(Math.random() * 100);
+    console.log(`Pregunta: ${number}`);
+    const userAnswer = readlineSync.question('Tu respuesta: ').toLowerCase();
+
+    const correctAnswer = isEven(number) ? 'yes' : 'no';
+
+    if (userAnswer !== 'yes' && userAnswer !== 'no') {
+      console.log(`'${userAnswer}' es una respuesta incorrecta ;(. La respuesta correcta era '${correctAnswer}'.`);
+      console.log(`¡Intentémoslo de nuevo, ${name}!`);
+      return;
+    }
+
+    if (userAnswer === correctAnswer) {
+      console.log('¡Correcto!');
+      correctAnswers += 1;
+    } else {
+      console.log(`'${userAnswer}' es una respuesta incorrecta ;(. Larespuesta correcta era '${correctAnswer}'.`);
+      console.log(`¡Intentémoslo de nuevo, ${name}!`);
+      return;
+    }
+  }
+
+  console.log(`¡Felicidades, ${name}!`);
+};
+
+export default runEvenGame;
